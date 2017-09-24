b92e9ed240e6758c4e56bb7e86935e1a09f220df
Administrator@XTM-01701112049 <502583304@qq.com>
2017-09-24 14:42:50 +0800

    实现了数据变化时的事件冒泡

--- NuoMi/task5/Observer.js
+++ NuoMi/task5/Observer.js
@@ -14,38 +14,56 @@
 		},
 
 		//发布事件（触发）
-		emit: function(key) {
+		emit: function(path) {
 			var _this=this;
 			var callbackArgs=Array.prototype.slice.call(arguments,1);
-			var arr=this.events[key];
-			for(var i=0;i<arr.length;i++) {
-				_this.events[key][i].apply(this,callbackArgs);
+			if(path.length>0) {
+				for(var i=path.length-1;i>=0;i--) {
+					var arr=_this.events[path[i]];
+					if(arr) {
+						for(var j=0;j<arr.length;j++) {
+							arr[j].apply(this,callbackArgs);
+						}
+					}
+				}
+			}
+			else {
+				var arr=_this.events[path[0]];
+				for(var j=0;j<arr.length;j++) {
+							arr[j].apply(this,callbackArgs);
+				}
 			}
 		}
 	};
 
 	//观察者构造函数
-	function Observer(data) {
+	function Observer(data,path,eventBus) {
+		this.eventBus=eventBus||new Event();
+		this.path=path||[];//记录路径
 		this.data=data;
 		this.walk(data);
-		this.eventBus=new Event();
+	}
+
+	Observer.prototype.$watch=function(key,callback) {
+		this.eventBus.on(key,callback);
 	}
 
 	//递归遍历对象的每一个属性
 	//为每一个对象的每一个属性绑定getter和setter
 	Observer.prototype.walk=function(obj) {
 		var val;//属性值
+		var _this=this;
 		for(var key in obj) {
 			//用hasOwnProperty过滤继承属性
 			if(obj.hasOwnProperty(key)) {
 				val=obj[key];
-
 				//判断是否到最底层，属性值是否还是对象
 				if(typeof val==='object') {
-					new Observer(val);
+					_this.path.push(key);
+					new Observer(val,this.path,_this.eventBus);
 				}
 
-				this.convert(key,val);
+				_this.convert(key,val);
 			}
 		}
 	};
@@ -60,29 +78,22 @@
 				return val;
 			},
 			set: function(newVal) {
-				//如果设置的为对象，继续进入下一层
-				if(typeof newVal==="object")
-					new Observer(newVal);
-
 				console.log("你设置了"+key);
 				console.log("新的"+key+"="+newVal);
-				if(newVal==val)data
-					return;
-				//触发$watch监听到对象的处理函数
-				_this.eventBus.emit(key,newVal);
 				val=newVal;
+				//触发$watch监听到对象的处理函数
+				_this.eventBus.emit(_this.path,newVal);
+				//如果设置的为对象，继续进入下一层
+				if(typeof newVal==="object")
+					new Observer(newVal,_this.path,_this.eventBus);
 			}
-		})
+		});
 	};
 
-	Observer.prototype.$watch=function(key,callback) {
-		this.eventBus.on(key,callback);
-	}
-
 	var data={
-		basicInfo: {
-			name: "albert",
-			city: "Hangzhou"
+		name: {
+			firstname: "albert",
+			lastName: "xiao" 
 		},
 		age: 23
 	};
@@ -91,4 +102,8 @@
 
 	app.$watch("age",function(age) {
 		console.log(`我的年纪变了，现在已经是：${age}岁了`)
+	});
+
+	app.$watch("name",function(newName) {
+		console.log(`我的名字变了，可能是姓氏变了，也可能是名字变了。`);
 	});
