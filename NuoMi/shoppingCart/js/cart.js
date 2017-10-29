733b13d7ae1b21509407fd7e6da44d922b0eae8c
Administrator@XTM-01701112049 <502583304@qq.com>
2017-10-29 22:22:09 +0800

    搭建了node.js本地服务器

--- NuoMi/shoppingCart/js/cart.js
+++ NuoMi/shoppingCart/js/cart.js
@@ -1,15 +1,22 @@
-new Vue({
+var vm=new Vue({
   el: "#app",
   data: {
-
+    totalMoney: 0,
+    productsList: []
   },
   filters: {
 
   },
   mounted: function() {
-
+    this.cartView();
   },
   methods: {
-    
+    cartView: function() {
+      var _this=this;
+      this.$http.get("data/cartData.json",{"id":123}).then(function(res) {
+        _this.productsList=res.body.result.list;
+        _this.totalMoney=res.body.result.totalMoney;
+      });
+    }
   }
 });
