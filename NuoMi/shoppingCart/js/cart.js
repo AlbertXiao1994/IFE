32a17e67cfc30d455b6229627aa560083508c80d
Administrator@XTM-01701112049 <502583304@qq.com>
2017-10-29 23:58:35 +0800

    实现了购物车的全选与取消全选、增减商品的数量的功能

--- NuoMi/shoppingCart/js/cart.js
+++ NuoMi/shoppingCart/js/cart.js
@@ -2,21 +2,59 @@ var vm=new Vue({
   el: "#app",
   data: {
     totalMoney: 0,
-    productsList: []
+    productsList: [],
+    checkAllFlag: false
   },
   filters: {
-
+    formatMoney: function(value) {
+      return "¥ "+value.toFixed(2);
+    }
   },
   mounted: function() {
-    this.cartView();
+    this.$nextTick(function() {
+      this.cartView();
+    });
   },
   methods: {
     cartView: function() {
       var _this=this;
-      this.$http.get("data/cartData.json",{"id":123}).then(function(res) {
+      this.$http.get("data/cartData.json",{"id":123}).then(res=> {
         _this.productsList=res.body.result.list;
         _this.totalMoney=res.body.result.totalMoney;
       });
+    },
+    changeMoney: function(product,way) {
+      if(way>0) {
+        product.productQuantity++;
+      }
+      else if(product.productQuantity>1){
+        product.productQuantity--;
+      }
+    },
+    selectedProduct:function(item) {
+      if(typeof item.checked == "undefined") {
+        this.$set(item,"checked",true);
+      }
+      else {
+        item.checked=!item.checked;
+      }
+    },
+    checkAll: function(flag) {
+      this.checkAllFlag=flag;
+      var _this=this;
+      this.productsList.forEach(function(item) {
+        if(typeof item.checked == "undefined") {
+        _this.$set(item,"checked",_this.checkAllFlag);
+      }
+      else {
+        item.checked=_this.checkAllFlag;
+      }
+      })
     }
   }
+});
+
+
+Vue.filter("money",function(value,type) {
+  return value.toFixed(2)+type;
 });
