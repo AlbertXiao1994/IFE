cc3411e7cbf096b3665f148deea9dd722d8da9f8
Administrator@XTM-01701112049 <502583304@qq.com>
2017-10-31 19:55:03 +0800

    实现了删除和结算功能

--- NuoMi/shoppingCart/js/cart.js
+++ NuoMi/shoppingCart/js/cart.js
@@ -3,7 +3,8 @@ var vm=new Vue({
   data: {
     totalMoney: 0,
     productsList: [],
-    checkAllFlag: false
+    checkAllFlag: false,
+    delFlag: false,
   },
   filters: {
     formatMoney: function(value) {
@@ -20,16 +21,18 @@ var vm=new Vue({
       var _this=this;
       this.$http.get("data/cartData.json",{"id":123}).then(res=> {
         _this.productsList=res.body.result.list;
-        _this.totalMoney=res.body.result.totalMoney;
+        // _this.totalMoney=res.body.result.totalMoney;
       });
     },
-    changeMoney: function(product,way) {
+    changeQuantity: function(product,way) {
       if(way>0) {
         product.productQuantity++;
       }
       else if(product.productQuantity>1){
         product.productQuantity--;
       }
+      //计算总金额
+      this.calTotalMoney();
     },
     selectedProduct:function(item) {
       if(typeof item.checked == "undefined") {
@@ -38,19 +41,38 @@ var vm=new Vue({
       else {
         item.checked=!item.checked;
       }
+      //计算总金额
+      this.calTotalMoney();
     },
     checkAll: function(flag) {
       this.checkAllFlag=flag;
       var _this=this;
       this.productsList.forEach(function(item) {
         if(typeof item.checked == "undefined") {
-        _this.$set(item,"checked",_this.checkAllFlag);
+          _this.$set(item,"checked",_this.checkAllFlag);
       }
       else {
         item.checked=_this.checkAllFlag;
       }
-      })
-    }
+      //计算总金额
+      _this.calTotalMoney();
+      });
+    },
+    calTotalMoney: function() {
+      var _this=this;
+      this.totalMoney=0;
+      this.productsList.forEach(function(item,index) {
+        if(item.checked) {
+          _this.totalMoney+=item.productQuantity*item.productPrice;
+      }
+      });
+    },
+    delConfirm: function(item) {
+      this.delFlag=false;
+      var index=this.productsList.indexOf(item);
+      this.productsList.splice(index,1);
+      this.calTotalMoney();
+    },
   }
 });
 
