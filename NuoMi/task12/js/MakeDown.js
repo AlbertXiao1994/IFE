b6d32730c81df633d73d63059df54efdf93d3ab7
Administrator@XTM-01701112049 <502583304@qq.com>
2017-11-06 20:50:28 +0800

    实现了解析标题、图片、链接、引用

--- NuoMi/task12/js/MakeDown.js
+++ NuoMi/task12/js/MakeDown.js
@@ -52,19 +52,66 @@ new Vue({
 		renderWords: _.debounce(
 		function() {
 				var parent=document.getElementById("rightBar");//右栏
+				parent.innerHTML="";//清空内容
 
 				//正则表达式
-				var regex=/^#{1,6} +.+\s*$/gm;
+				var input=/^.+\s*$/gm;
+				var heading=/#{1,6} +.+\s*/;
+				var cite=/^> +.+\s*$/;
+				var link=/\[.+\]\(.+\)/;
+				var img=/!\[.+\]\(.+\)/;
 
-				var result=this.inputWords.match(regex);
+				var result=this.inputWords.match(input);
 				if(result !=null) {
-					parent.innerHTML="";//清空内容
 					result.forEach(function(x) {
-						var p = document.createElement("h1");
-						var text=x;
-    					p.innerText = ""+text+"";
-     					// 添加到body元素里
-     					parent.appendChild(p);
+						if(x.match(heading)!=null) {
+							var i=x.toString().search(/ /);
+							var p;
+							if(i==1)
+								p = document.createElement("h1");
+							else if(i==2)
+								p = document.createElement("h2");
+							else if(i==3)
+								p = document.createElement("h3");
+							else if(i=4)
+								p = document.createElement("h4");
+							else if(i=5)
+								p = document.createElement("h5");
+							else if(i=6)
+								p = document.createElement("h6");
+    						p.innerText = ""+x.toString().slice(i+1)+"";
+     						// 添加到body元素里
+     						parent.appendChild(p);
+     					}
+     					else if(x.match(cite)!=null) {
+     						var i=x.toString().search(/ /);
+							var d = document.createElement("div");
+							d.classList.add("cite");
+    						d.innerText = ""+x.toString().slice(i+1)+"";
+     						// 添加到body元素里
+     						parent.appendChild(d);
+     					}
+     					else if(x.match(img)!=null) {
+							var i = document.createElement("img");
+							var s=x.toString().search(/\(/);
+							i.src=x.toString().slice(s+1,-1);
+							var s1=x.toString().search(/\[/);
+							var e1=x.toString().search(/\]/);
+    						i.alt = x.toString().slice(s1+1,e1);
+     						// 添加到body元素里
+     						parent.appendChild(i);
+     					}
+     					else if(x.match(link)!=null) {
+							var a = document.createElement("a");
+							var s=x.toString().search(/\(/);
+							a.href=x.toString().slice(s+1,-1);
+							var s1=x.toString().search(/\[/);
+							var e1=x.toString().search(/\]/);
+    						a.innerText = x.toString().slice(s1+1,e1);
+    						a.target="_blank";
+     						// 添加到body元素里
+     						parent.appendChild(a);
+     					}
 					});
 				}		
 			},
