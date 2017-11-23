feb85f73e75d9f1b97c997489e5af55e84e62656
Administrator@XTM-01701112049 <502583304@qq.com>
2017-11-23 23:52:54 +0800

    新建了一个项目

---
+++ NuoMi/task17/task.js
@@ -0,0 +1,15 @@
+function Timer() {
+  this.s1 = 0;
+  this.s2 = 0;
+  // 箭头函数
+  setInterval(() => this.s1++, 1000);
+  // 普通函数
+  setInterval(function () {
+    this.s2++;
+  }, 1000);
+}
+
+var timer = new Timer();
+
+setTimeout(() => console.log('s1: ', timer.s1), 3100);
+setTimeout(() => console.log('s2: ', timer.s2), 3100);
