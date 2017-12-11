24a11bc69fdc3d4d5fcfa0cf64e5728ab2c16883
Administrator@XTM-01701112049 <502583304@qq.com>
2017-12-11 22:30:09 +0800

    文件打包

---
+++ NuoMi/sell-app/src/components/shopCart/shopCart.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="shopCart">
+    
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+
+  },
+  computed: {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.shopCart {
+  width: 100%;
+  height: 48px;
+}
+</style>
