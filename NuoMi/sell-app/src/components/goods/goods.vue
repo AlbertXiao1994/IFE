5a14351050d89808b35fea0e1264114c651be972
Administrator@XTM-01701112049 <502583304@qq.com>
2017-12-11 11:37:48 +0800

    实现了商品列表部分的样式，使用三方插件better-scroll实现滚动

--- NuoMi/sell-app/src/components/goods/goods.vue
+++ NuoMi/sell-app/src/components/goods/goods.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="goods">
-    <div class="menu-wrapper">
+    <div class="menu-wrapper" ref="menuWrapper">
       <ul class="menu">
         <li v-for="(item,index) in goods" class="menu-item" 
         :class="{selected: index==selectIndex}" @click="selectOption(index)">
@@ -9,21 +9,24 @@
         </li>
       </ul>
     </div>
-    <div class="goods-wrapper">
+    <div class="goods-wrapper" ref="goodsWrapper">
       <ul>
         <li v-for="item in goods" class="food-list">
           <h1 class="header">{{ item.name }}</h1>
-          <ul>
+          <ul class="food-content">
             <li v-for="food in item.foods" class="food-item">
               <div class="icon">
-                <img :src="food.icon">
+                <img :src="food.icon" width="57" height="57">
               </div>
               <div class="content">
                 <h2 class="name">{{ food.name }}</h2>
                 <p class="description">{{ food.description }}</p>
                 <div class=extra>
-                  <span class="sellCount">月售{{ food.sellCount }}份</span>
-                  <span class="rating">好评率{{ food.rating }}%</span>
+                  <span class="sellCount">月售{{ food.sellCount }}份</span><span class="rating">好评率{{ food.rating }}%</span>
+                </div>
+                <div class="price">
+                  <span class="nowPrice">¥{{ food.price }}</span>
+                  <span class="oldPrice" v-if="food.oldPrice">¥{{ food.oldPrice }}<span class="line"></span></span>
                 </div>
               </div>
             </li>
@@ -36,6 +39,7 @@
 
 <script>
 import icon from '../icon/icon'
+import BScroll from 'better-scroll'
 export default {
   components: {
     icon
@@ -50,6 +54,9 @@ export default {
     this.$http.get('/api/goods')
     .then((res) => {
       this.goods = res.data
+      this.$nextTick(() => {
+        this._initScroll()
+      })
     }, (err) => {
       console.log(err)
     })
@@ -57,6 +64,10 @@ export default {
   methods: {
     selectOption (index) {
       this.selectIndex = index
+    },
+    _initScroll () {
+      this.menuScroll = new BScroll(this.$refs.menuWrapper, {})
+      this.goodsScroll = new BScroll(this.$refs.goodsWrapper, {})
     }
   }
 }
@@ -111,6 +122,76 @@ export default {
   background: #fff;
 }
 .goods-wrapper {
-  overflow: auto;
+  flex: 1;
+}
+.header {
+  width: 100%;
+  height: 26px;
+  background: #f3f5f7;
+  border-left: 3px solid #d9dde1;
+  font-size: 12px;
+  color: rgb(147,153,159);
+  line-height: 26px;
+  padding-left: 14px;
+}
+.food-content {
+  padding-left: 18px;
+  padding-right: 18px;
+}
+.food-item {
+  display: flex;
+  padding: 18px 0;
+  border-bottom: 1px solid rgba(7,17,27,0.1);
+}
+.food-item:last-child {
+  border-width: 0;
+}
+.food-item .icon {
+  flex: 0 0 57px;
+  margin-right: 10px;
+}
+.food-item .content {
+  flex: 1;
+  padding-top: 2px;
+}
+.content .name {
+  font-size: 14px;
+  color: rgb(7,17,27);
+  line-height: 14px;
+}
+.content .description,.extra {
+  font-size: 10px;
+  color: rgb(147,153,159);
+  line-height: 10px;
+  margin-top: 8px;
+}
+.content .price {
+  margin-top: 8px;
+}
+.nowPrice {
+  font-size: 14px;
+  color: rgb(240,20,20);
+  font-weight: 700;
+  line-height: 24px;
+}
+.oldPrice {
+  position: relative;
+  font-size: 10px;
+  color: rgb(147,153,159);
+  font-weight: 700;
+  line-height: 24px;
+  margin-left: 8px;
+}
+.oldPrice .line {
+  display: inline-block;
+  width: 100%;
+  height: 1px;
+  position: absolute;
+  left: 0;
+  bottom: 6px;
+  border-bottom: 1px solid rgb(147,153,159);
+}
+.extra .sellCount {
+  margin-right: 12px;
 }
 </style>
