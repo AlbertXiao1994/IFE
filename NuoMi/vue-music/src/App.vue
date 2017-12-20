13b512c03a82af6259a82c7e3a112d22ed26de45
Administrator@XTM-01701112049 <502583304@qq.com>
2017-12-21 00:08:12 +0800

    使用vue-cli搭建项目，组织src目录

---
+++ NuoMi/vue-music/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    hello world
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style lang="scss" rel="stylesheet/scss">
+	@import 'common/sass/variable';
+
+	#app {
+		color: $color-theme;
+	}
+</style>
